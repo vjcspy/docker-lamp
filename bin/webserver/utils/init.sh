70027b27bbc89022eeb0454bff2e8140cfb98777
Khôi Lê <mr.vjcspy@gmail.com>
2019-04-18 00:04:39 +0700

    - refactor

--- bin/webserver/utils/init.sh
+++ bin/webserver/utils/init.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
+echo "start server"
 
-source /etc/apache2/envvars
-exec apache2 -D FOREGROUND
+# enable all site
+for filename in /etc/apache2/sites-available/*.conf; do
+    if [[ $(basename ${filename}) != "000-default.conf" ]] && [[ $(basename ${filename}) != "default-ssl.conf" ]] && [[ $(basename ${filename}) != "default.conf" ]]
+     then
+        a2ensite $(basename ${filename})
+    fi
+done
+
+# setup debug cli
+export XDEBUG_CONFIG="remote_enable=1 remote_mode=req remote_port=9000 remote_host=docker.for.mac.localhost remote_connect_back=0"
+#export PHP_IDE_CONFIG="serverName=mage2"
+
+phpdismod xdebug
+
+echo "start apache"
+exec apachectl -D FOREGROUND
+
