9ae9d2c20f3b5fc3eec1ece52ea05c97e6ea56a3
vjcspy <mr.vjcspy@gmail.com>
2019-11-23 00:52:40 -0800

    - remove xdebug setting

--- bin/webserver/utils/up.sh
+++ bin/webserver/utils/up.sh
@@ -9,7 +9,7 @@ for filename in /etc/apache2/sites-available/*.conf; do
 done
 
 # setup debug cli
-export XDEBUG_CONFIG="remote_enable=1 remote_mode=req remote_port=9000 remote_host=host.docker.internal remote_connect_back=0"
+#export XDEBUG_CONFIG="remote_enable=1 remote_mode=req remote_port=9000 remote_host=host.docker.internal remote_connect_back=0"
 #export PHP_IDE_CONFIG="serverName=mage2"
 
 phpdismod xdebug
