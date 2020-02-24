2563e834fc9816c3b22df00e1a2aa4bebadbb8ec
vjcspy <mr.vjcspy@gmail.com>
2020-02-24 10:03:11 +0700

    done all

--- bin/webserver/utils/init.sh
+++ bin/webserver/utils/init.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
-echo "start server"
 
-# enable all site
-for filename in /etc/apache2/sites-available/*.conf; do
-    if [[ $(basename ${filename}) != "000-default.conf" ]] && [[ $(basename ${filename}) != "default-ssl.conf" ]] && [[ $(basename ${filename}) != "default.conf" ]]
-     then
-        a2ensite $(basename ${filename})
-    fi
-done
-
-# config hosts
 HOST_DOMAIN="host.docker.internal"
 ping -q -c1 $HOST_DOMAIN > /dev/null 2>&1
 if [ $? -ne 0 ]; then
@@ -17,12 +7,4 @@ if [ $? -ne 0 ]; then
   echo -e "$HOST_IP\t$HOST_DOMAIN" >> /etc/hosts
 fi
 
-# setup debug cli
-#export XDEBUG_CONFIG="remote_enable=1 remote_mode=req remote_port=9000 remote_host=host.docker.internal remote_connect_back=0"
-#export PHP_IDE_CONFIG="serverName=mage2"
-
-phpdismod xdebug
-
-echo "start apache"
-exec apachectl -D FOREGROUND
 
